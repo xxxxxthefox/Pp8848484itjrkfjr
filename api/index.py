92ed9f9d057e284385a1011874f0dc9a658fcfe5
fox <xxxxxthefox@gmail.com>
2026-03-11 20:59:20 +0300

    Auto-Deploy: api/index.py

---
+++ api/index.py
@@ -0,0 +1,24 @@
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import sys, io, contextlib
+
+app = FastAPI()
+
+class CodeReq(BaseModel):
+    user: str
+    code: str
+    key: str
+
+@app.post("/api/execute")
+async def execute(data: CodeReq):
+    if data.key != "FOX_SECRET_2026":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+    f = io.StringIO()
+    try:
+        with contextlib.redirect_stdout(f):
+            exec(data.code)
+        return {"output": f.getvalue()}
+    except Exception as e:
+        return {"output": str(e)}
